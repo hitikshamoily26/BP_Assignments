b22deb6776ff42a903f25c47d55ebef409327612
hitikshamoily26 <55343391+hitikshamoily26@users.noreply.github.com>
2021-06-10 22:19:22 +0530

    Add files via upload

---
+++ PalindromeOrNot.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class PalindromeOrNot
+{
+ 
+   static boolean isPalindrome(String str){
+   
+     int i = 0,j = str.length()-1;
+  
+    
+     while(i<j){
+           
+ 	if(str.charAt(i)!=str.charAt(j))
+       
+         return false;
+            
+     
+     i++;
+    
+     j--;
+    
+    }
+ 
+     
+  return true;
+  
+  }
+    
+	
+	public static void main(String[] args) {
+
+	    Scanner s = new Scanner(System.in);
+	
+	    System.out.println("Enter a String to check whether it is palindrome or not");
+	
+	    String word = s.next();
+
+		
+		
+	    if(isPalindrome(word)){
+		 
+   		System.out.println(word+ " is a palindrome.");
+	
+	    }
+		
+	    else{
+		   
+            System.out.println(word+ " is not a palindrome.");
+	
+	    }
+
+	
+	}
+
+}
